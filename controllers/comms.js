bf15d97743f864ba15387710764f1d93b7932878
Mark  Healy <markhealy@hiot.io>
2018-01-02 16:03:03 +0000

    receive comments from homepage

---
+++ controllers/comms.js
@@ -0,0 +1,22 @@
+var Comms = require('../models/comms')
+
+exports.commsList=function(){
+
+}
+
+exports.newComms=function(req,res,next){
+     var comms = new Comms({
+    email: req.body.contact.email,
+    name: req.body.contact.name,
+    comment: req.body.contact.comment,
+    responded: false,
+    added: new Date()
+  })
+  comms.save(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.sendStatus(200)
+  })
+
+}
--- routes/no_auth.js
+++ routes/no_auth.js
@@ -2,7 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 var profileController = require('../controllers/profile')
+var commsController = require('../controllers/comms')
 router.post('/auth', profileController.profile_auth) // login
 router.post('/register', profileController.profile_create) // register
-
+router.post('/contact',commsController.newComms) //contact us link
 module.exports = router
